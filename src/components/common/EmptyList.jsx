45a262297db8d7fdf20606150254b046cd079711
habi <habtemariamshumye46@gmail.com>
2022-12-19 15:14:17 +0300

    adding gif

--- src/components/common/EmptyList.jsx
+++ src/components/common/EmptyList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import empty from "../../assets/search.gif";
+
 
 const EmptyList = () => {
   return (
     <>
-      <div className="emptyList-wrap">
-        <img src="https://tenor.com/view/sleepy-gif-25788249" alt="empty" />
+      <div className="grid items-center justify-center font-semibold text-3xl pb-20 text-orange-500 font-serif">
+        <img src={empty} alt="empty" className="font-sm"/>
+        <h1>
+          Not Found
+          </h1>
       </div>
     </>
   );
